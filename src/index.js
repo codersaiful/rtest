9572b7fe5e3af4a35772c602c36f8e4a48fb64dd
Saiful Islam <codersaiful@gmail.com>
2021-06-10 16:17:33 +0600

    Ready for 1.0.2

--- src/index.js
+++ src/index.js
@@ -1,48 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './common.css';
+
 import App from './App';
 
-import Header from './my-folder/header'
 
 import reportWebVitals from './reportWebVitals';
 
-const myTable = (
-  <table>
-    <tr>
-      <th>Name</th>
-      <th>Address</th>
-    </tr>
-    <tr>
-      <td>Saiful Islam</td>
-      <td>Rajshahi, Bangladesh</td>
-    </tr>    
-  </table>
-);
-ReactDOM.render(myTable,document.getElementById('another-root'));
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Header />
-//     <App name="Saiful"/>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Header />
-    <p>Welcome To you. How are u.</p>
-    <App name="Saiful Islam"/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 
 const myElement = React.createElement('h1',{},'Hello Bangladesh. How are u?');
