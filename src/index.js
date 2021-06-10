7fa96815e3c780960ff909c8a002285427f6485c
Saiful Islam <codersaiful@gmail.com>
2021-06-10 16:07:56 +0600

    now will create new commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,46 @@ import App from './App';
 import Header from './my-folder/header'
 
 import reportWebVitals from './reportWebVitals';
+
+const myTable = (
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Address</th>
+    </tr>
+    <tr>
+      <td>Saiful Islam</td>
+      <td>Rajshahi, Bangladesh</td>
+    </tr>
+    <tr>
+      <td>Saiful Islam 1</td>
+      <td>Rajshahi, Bangladesh</td>
+    </tr>
+    <tr>
+      <td>Saiful Islam 2</td>
+      <td>Rajshahi, Bangladesh</td>
+    </tr>
+    <tr>
+      <td>Saiful Islam 3</td>
+      <td>Rajshahi, Bangladesh</td>
+    </tr>
+    
+  </table>
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Header />
+//     <App name="Saiful"/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <App />
+    <p>Welcome To you. How are u.</p>
+    <App name="Saiful Islam"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
